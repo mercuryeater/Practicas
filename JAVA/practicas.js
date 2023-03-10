0f7a5d9bae13fc7ab13b8a7eb40c7dc3219a956e
Federico <federicoc03@gmail.com>
2023-03-10 16:32:36 -0500

    Quedé en funciones, apuntes en el archivo js

---
+++ JAVA/practicas.js
@@ -0,0 +1,124 @@
+/* 
+
+console.log("Hola Mundo"); 
+
+//Comentario de 1 sola linea
+/*
+Este
+Es
+Un
+Comentario
+De
+Varias
+Lineas
+
+
+console.log("Esto está muy bacano");
+
+//Probando la combinacion de comillas
+console.log('Y \'él dijo\': "Hola mundo"');
+
+console.log("Hola " + "Mundo " + "Cómo " + "Estás");
+
+//ahora numeros
+console.log(1 + 2)
+console.log(3 * 4 + 5)
+console.log(3 * (4 + 5))
+console.log(8 / 2)
+
+*/
+
+
+//VARIABLES:
+
+/*
+let name = "Federico";
+console.log("Hola " + name);
+
+
+let side = 10;
+
+console.log("El perímetro de un cuadrado de lado " + side + " es " + (side * 4));
+console.log("El área de un cuadrado de lado " + side + " es " + (side * side));
+
+*/
+
+//CONDICIONALES
+/*
+if (true) {
+    console.log("Hola Mundo");
+  }
+
+
+  let num = 18;
+
+  if (num < 10) {
+    console.log("El número es menor a 10");
+  } else {
+    console.log("El número es igual o mayor a 10");
+  }
+
+
+
+
+        //CONDICION ANIDADA
+
+  let num = 8;
+
+if (num < 10) {
+  console.log("El número es menor a 10");
+} else {
+  if (num > 10) {
+    console.log("El número es mayor a 10");
+  } else {
+    console.log("El número es igual a 10");
+  }
+}
+  
+
+    //ELSE IF
+
+let num = 12;
+
+if (num < 10) {
+  console.log("El número es menor a 10");
+} else if (num > 10) {
+  console.log("El número es mayor a 10");
+} else {
+  console.log("El número es igual a 10");
+}
+
+
+
+//OPERADOR TERNARIO
+
+let num = 15;
+
+num >= 15 ? console.log('Es mayor o igual que 15') : console.log('Es menor que 15');\
+
+
+//CICLOS
+
+let i = 0;
+while (i < 850) {
+    console.log(i +" Hola mundo");
+    i++
+}
+
+//Ciclo infinito:
+while (true) {
+    console.log("Hola ")
+}
+
+
+
+// EL FOR
+
+for (let i = 0; i < 850; i ++) {
+    console.log(i + " Hola mundo");
+  }
+
+for (let i =100; i >0; i--) {
+    console.log(i);
+}
+*/
